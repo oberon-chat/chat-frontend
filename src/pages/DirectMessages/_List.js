1ad2c092a9577b15715a0d2bd2fd30e980547158
Chris Laskey <contact@chrislaskey.com>
2017-10-25 19:26:23 -0400

    Add status dot to new direct message user list

--- src/pages/DirectMessages/_List.js
+++ src/pages/DirectMessages/_List.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { map } from 'lodash'
+import { getIsConnected } from '../../reducers/connectedUsers'
+import UserConnectivityDot from '../Users/_ConnectivityDot'
 import { Button } from 'antd'
 
-const UsersList = ({ handleClick, users }) => {
+const UsersList = ({ handleClick, isConnected, users }) => {
   const renderUser = (user) => {
     return (
       <li key={user.id}>
-        <h3>{user.name}</h3>
+        <h3>
+          <UserConnectivityDot isConnected={isConnected(user)} />
+          {user.name}
+        </h3>
         <Button icon='arrow-right' onClick={() => handleClick(user)}>
           Open
         </Button>
@@ -27,7 +32,7 @@ const UsersList = ({ handleClick, users }) => {
 UsersList.displayName = 'UsersList'
 
 const mapStateToProps = (state) => ({
-
+  isConnected: (user) => getIsConnected(state, user.id)
 })
 
 const mapDispatchToProps = (dispatch) => ({
